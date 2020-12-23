7682e06ea8b584b787460a5ab5d46b4912719df6
birane <birane906@gmail.com>
2020-12-23 13:13:37 +0100

    docs : comments added style : translation in french made

--- prototypes/src/gui/controller/LoginController.java
+++ prototypes/src/gui/controller/LoginController.java
@@ -27,20 +27,35 @@ public class LoginController implements Initializable{
 	 */
 	private UserFacade userFacade;
 
+	/**
+	 * Description of the label in the case of a wrong authentification
+	 */
 	@FXML
-	private Label authentificationIncorrecte;
+	private Label wrongAuthentification;
 
+	/**
+	 * Description of the close button
+	 */
 	@FXML
-	private Button fermerButton;
+	private Button closeButton;
 
+	/**
+	 * Description of our application logo
+	 */
 	@FXML
 	private ImageView logoImage;
 
+	/**
+	 * Description of the email text field
+	 */
 	@FXML
 	private TextField emailTextField;
 
+	/**
+	 * Description of the password text field
+	 */
 	@FXML
-	private PasswordField motDePassePasswordField;
+	private PasswordField passwordField;
 
 
 	/**
@@ -52,30 +67,40 @@ public class LoginController implements Initializable{
 
 
 	/**
-	 * When i click the red button, my window is closed
+	 * Method which permite to close the window when you click on the close button
 	 */
-	public void fermerButtonOnAction(){
-		Stage stage = (Stage) fermerButton.getScene().getWindow();
+	public void closeButtonOnAction(){
+		Stage stage = (Stage) closeButton.getScene().getWindow();
 		stage.close();
 	}
 
+	/**
+	 * Method which permite to connect into TeamPoint when the user clicks on the login button and provides the right
+	 * email and password
+	 */
 	@FXML
-	public void seConnecterButtonOnAction(ActionEvent event){
+	public void loginButtonOnAction(ActionEvent event){
 		if(emailTextField.getText().isBlank() 
-			|| motDePassePasswordField.getText().isBlank() 
-			|| !userFacade.login(emailTextField.getText(),motDePassePasswordField.getText())){
-			authIncorrecte();
+			|| passwordField.getText().isBlank()
+			|| !userFacade.login(emailTextField.getText(),passwordField.getText())){
+			wrongAuth();
 		}else{
 			//CHANGEMENT DE VUE
 			System.out.println("validé");
 		}
 	}
 
+	/**
+	 * an utility function used
+	 */
 	@FXML
-	public void authIncorrecte(){
-		authentificationIncorrecte.setText("Email ou mot de passe incorrect");
+	private void wrongAuth(){
+		wrongAuthentification.setText("Email ou mot de passe incorrect");
 	}
 
+	/**
+	 * Method which initialize the login window
+	 */
 	@Override
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		File brandingFile = new File("../teampoint_logo/TeamPointLogo2.png");
