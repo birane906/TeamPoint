e6d9789133265f7aaa1b6ff5744743a99d0430be
Galois Nicolas <galoisnicolas@gmail.com>
2020-12-29 13:06:39 +0100

    fix database script

--- database/teampoint.sql
+++ database/teampoint.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  mar. 29 déc. 2020 à 12:20
+-- Généré le :  mar. 29 déc. 2020 à 13:05
 -- Version du serveur :  10.4.8-MariaDB
 -- Version de PHP :  7.3.10
 
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `board` (
   `idBoard` int(10) NOT NULL,
+  `userOwner` int(10) NOT NULL,
   `boardName` varchar(25) NOT NULL,
   `parentWorkspace` int(10) NOT NULL,
   `boardCreationDate` date NOT NULL DEFAULT current_timestamp()
@@ -55,6 +56,9 @@ CREATE TABLE `board_contains` (
 
 CREATE TABLE `cell` (
   `idCell` int(10) NOT NULL,
+  `idBoard` int(10) NOT NULL,
+  `idColumn` int(10) NOT NULL,
+  `idItemCollection` int(10) NOT NULL,
   `cellValue` varchar(25) NOT NULL,
   `idCellType` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -79,6 +83,7 @@ CREATE TABLE `celltype` (
 
 CREATE TABLE `column` (
   `idColumn` int(10) NOT NULL,
+  `idBoard` int(10) NOT NULL,
   `columnName` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -90,6 +95,8 @@ CREATE TABLE `column` (
 
 CREATE TABLE `item` (
   `idItem` int(10) NOT NULL,
+  `idBoard` int(10) NOT NULL,
+  `idItemCollection` int(10) NOT NULL,
   `itemName` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -101,30 +108,19 @@ CREATE TABLE `item` (
 
 CREATE TABLE `itemcollection` (
   `idItemCollection` int(10) NOT NULL,
+  `idBoard` int(10) NOT NULL,
   `itemCollectionName` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `itemcollection_contains_items`
+-- Structure de la table `item_collection_item`
 --
 
-CREATE TABLE `itemcollection_contains_items` (
+CREATE TABLE `item_collection_item` (
   `idItemCollection` int(10) NOT NULL,
   `idItem` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `item_column_crossed_cell`
---
-
-CREATE TABLE `item_column_crossed_cell` (
-  `idColumn` int(10) NOT NULL,
-  `idItem` int(10) NOT NULL,
-  `idCell` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -181,6 +177,7 @@ CREATE TABLE `user_attendance` (
 
 CREATE TABLE `workspace` (
   `idWorskpace` int(10) NOT NULL,
+  `idUserOwner` int(10) NOT NULL,
   `workspaceName` varchar(25) NOT NULL,
   `workspaceCreationDate` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -194,7 +191,8 @@ CREATE TABLE `workspace` (
 --
 ALTER TABLE `board`
   ADD PRIMARY KEY (`idBoard`),
-  ADD KEY `FOREIGN_PARENT_WORKSPACE` (`parentWorkspace`);
+  ADD KEY `FOREIGN_PARENT_WORKSPACE` (`parentWorkspace`),
+  ADD KEY `FOREIGN_USER_OWNER` (`userOwner`);
 
 --
 -- Index pour la table `board_contains`
@@ -209,7 +207,10 @@ ALTER TABLE `board_contains`
 --
 ALTER TABLE `cell`
   ADD PRIMARY KEY (`idCell`),
-  ADD KEY `FOREIGN_CELL_TYPE` (`idCellType`);
+  ADD KEY `FOREIGN_CELL_TYPE` (`idCellType`),
+  ADD KEY `FOREIGN_CELL_BOARD` (`idBoard`),
+  ADD KEY `FOREIGN_COLUMN_CELL` (`idColumn`),
+  ADD KEY `FOREIGN_ITEM_COLLECTION_CELL` (`idItemCollection`);
 
 --
 -- Index pour la table `celltype`
@@ -221,34 +222,30 @@ ALTER TABLE `celltype`
 -- Index pour la table `column`
 --
 ALTER TABLE `column`
-  ADD PRIMARY KEY (`idColumn`);
+  ADD PRIMARY KEY (`idColumn`),
+  ADD KEY `FOREIGN_COLUMN_BOARD_KEY` (`idBoard`);
 
 --
 -- Index pour la table `item`
 --
 ALTER TABLE `item`
-  ADD PRIMARY KEY (`idItem`);
+  ADD PRIMARY KEY (`idItem`),
+  ADD KEY `FOREIGN_BOARD_ITEM_KEY` (`idBoard`),
+  ADD KEY `FOREIGN_ITEM_COLLECTION_ITEM_KEY` (`idItemCollection`);
 
 --
 -- Index pour la table `itemcollection`
 --
 ALTER TABLE `itemcollection`
-  ADD PRIMARY KEY (`idItemCollection`);
+  ADD PRIMARY KEY (`idItemCollection`),
+  ADD KEY `FOREIGN_BOARD_ITEM_COLLECTION_KEY` (`idBoard`);
 
 --
--- Index pour la table `itemcollection_contains_items`
+-- Index pour la table `item_collection_item`
 --
-ALTER TABLE `itemcollection_contains_items`
-  ADD KEY `FOREIGN_ITEM_COLLECTION` (`idItemCollection`),
-  ADD KEY `FOREIGN_ITEM_BELONGS_TO` (`idItem`);
-
---
--- Index pour la table `item_column_crossed_cell`
---
-ALTER TABLE `item_column_crossed_cell`
-  ADD KEY `FOREIGN_CELL` (`idCell`),
-  ADD KEY `FOREIGN_COLUMN` (`idColumn`),
-  ADD KEY `FOREIGN_ITEM` (`idItem`);
+ALTER TABLE `item_collection_item`
+  ADD KEY `FOREIGN_ITEM__ITEM_COLLECTION` (`idItem`),
+  ADD KEY `FOREIGN_ITEM_COLLECTION_ITEM` (`idItemCollection`);
 
 --
 -- Index pour la table `user`
@@ -273,7 +270,8 @@ ALTER TABLE `user_attendance`
 -- Index pour la table `workspace`
 --
 ALTER TABLE `workspace`
-  ADD PRIMARY KEY (`idWorskpace`);
+  ADD PRIMARY KEY (`idWorskpace`),
+  ADD KEY `FOREIGN_ADMIN_OWNER_WORKSPACE` (`idUserOwner`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
@@ -293,7 +291,8 @@ ALTER TABLE `user`
 -- Contraintes pour la table `board`
 --
 ALTER TABLE `board`
-  ADD CONSTRAINT `FOREIGN_PARENT_WORKSPACE` FOREIGN KEY (`parentWorkspace`) REFERENCES `workspace` (`idWorskpace`);
+  ADD CONSTRAINT `FOREIGN_PARENT_WORKSPACE` FOREIGN KEY (`parentWorkspace`) REFERENCES `workspace` (`idWorskpace`),
+  ADD CONSTRAINT `FOREIGN_USER_OWNER` FOREIGN KEY (`userOwner`) REFERENCES `user` (`idUser`);
 
 --
 -- Contraintes pour la table `board_contains`
@@ -307,22 +306,37 @@ ALTER TABLE `board_contains`
 -- Contraintes pour la table `cell`
 --
 ALTER TABLE `cell`
-  ADD CONSTRAINT `FOREIGN_CELL_TYPE` FOREIGN KEY (`idCellType`) REFERENCES `celltype` (`idCellType`);
+  ADD CONSTRAINT `FOREIGN_CELL_BOARD` FOREIGN KEY (`idBoard`) REFERENCES `board` (`idBoard`),
+  ADD CONSTRAINT `FOREIGN_CELL_TYPE` FOREIGN KEY (`idCellType`) REFERENCES `celltype` (`idCellType`),
+  ADD CONSTRAINT `FOREIGN_COLUMN_CELL` FOREIGN KEY (`idColumn`) REFERENCES `column` (`idColumn`),
+  ADD CONSTRAINT `FOREIGN_ITEM_COLLECTION_CELL` FOREIGN KEY (`idItemCollection`) REFERENCES `itemcollection` (`idItemCollection`);
 
 --
--- Contraintes pour la table `itemcollection_contains_items`
+-- Contraintes pour la table `column`
 --
-ALTER TABLE `itemcollection_contains_items`
-  ADD CONSTRAINT `FOREIGN_ITEM_BELONGS_TO` FOREIGN KEY (`idItem`) REFERENCES `item` (`idItem`),
-  ADD CONSTRAINT `FOREIGN_ITEM_COLLECTION` FOREIGN KEY (`idItemCollection`) REFERENCES `itemcollection` (`idItemCollection`);
+ALTER TABLE `column`
+  ADD CONSTRAINT `FOREIGN_COLUMN_BOARD_KEY` FOREIGN KEY (`idBoard`) REFERENCES `board` (`idBoard`);
 
 --
--- Contraintes pour la table `item_column_crossed_cell`
+-- Contraintes pour la table `item`
 --
-ALTER TABLE `item_column_crossed_cell`
-  ADD CONSTRAINT `FOREIGN_CELL` FOREIGN KEY (`idCell`) REFERENCES `cell` (`idCell`),
-  ADD CONSTRAINT `FOREIGN_COLUMN` FOREIGN KEY (`idColumn`) REFERENCES `column` (`idColumn`),
-  ADD CONSTRAINT `FOREIGN_ITEM` FOREIGN KEY (`idItem`) REFERENCES `item` (`idItem`);
+ALTER TABLE `item`
+  ADD CONSTRAINT `FOREIGN_BOARD_ITEM` FOREIGN KEY (`idBoard`) REFERENCES `board` (`idBoard`),
+  ADD CONSTRAINT `FOREIGN_BOARD_ITEM_KEY` FOREIGN KEY (`idBoard`) REFERENCES `board` (`idBoard`),
+  ADD CONSTRAINT `FOREIGN_ITEM_COLLECTION_ITEM_KEY` FOREIGN KEY (`idItemCollection`) REFERENCES `itemcollection` (`idItemCollection`);
+
+--
+-- Contraintes pour la table `itemcollection`
+--
+ALTER TABLE `itemcollection`
+  ADD CONSTRAINT `FOREIGN_BOARD_ITEM_COLLECTION_KEY` FOREIGN KEY (`idBoard`) REFERENCES `board` (`idBoard`);
+
+--
+-- Contraintes pour la table `item_collection_item`
+--
+ALTER TABLE `item_collection_item`
+  ADD CONSTRAINT `FOREIGN_ITEM_COLLECTION_ITEM` FOREIGN KEY (`idItemCollection`) REFERENCES `itemcollection` (`idItemCollection`),
+  ADD CONSTRAINT `FOREIGN_ITEM__ITEM_COLLECTION` FOREIGN KEY (`idItem`) REFERENCES `item` (`idItem`);
 
 --
 -- Contraintes pour la table `user_as_member_of_workspace`
@@ -336,6 +350,12 @@ ALTER TABLE `user_as_member_of_workspace`
 --
 ALTER TABLE `user_attendance`
   ADD CONSTRAINT `FOREIGN_USER_ATTENDANCE` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`);
+
+--
+-- Contraintes pour la table `workspace`
+--
+ALTER TABLE `workspace`
+  ADD CONSTRAINT `FOREIGN_ADMIN_OWNER_WORKSPACE` FOREIGN KEY (`idUserOwner`) REFERENCES `user` (`idUser`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
