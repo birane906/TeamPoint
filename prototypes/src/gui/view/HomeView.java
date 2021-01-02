6c8baed96fabc77136c62d75df05b0d65fb9f3f8
Salim_Azh <azharhoussen.salim@gmail.com>
2021-01-02 15:44:53 +0100

    feat home view start

--- prototypes/src/gui/view/HomeView.java
+++ prototypes/src/gui/view/HomeView.java
@@ -1,5 +1,16 @@
 package gui.view;
 
-public class HomeView {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+public class HomeView {
+    public void start(Stage primaryStage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryStage.setScene(new Scene(root, 450, 500));
+		primaryStage.show();
+	}
 }
