5cdf03b4378cd6a42957b60b151acf219f69a57a
birane906 <birane906@gmail.com>
2021-01-02 20:37:45 +0100

    features : connection possible

--- prototypes/src/gui/controller/HomeController.java
+++ prototypes/src/gui/controller/HomeController.java
@@ -1,4 +1,64 @@
 package gui.controller;
 
-public class HomeController {
+
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import business_logic.UserFacade;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+/**
+ * Description of HomeController.
+ *
+ *
+ */
+public class HomeController{
+    /**
+     * Description of the property userFacade.
+     */
+    private UserFacade userFacade;
+
+
+    /**
+     * Description of
+     */
+    @FXML
+    private ImageView profileImage;
+
+
+    /**
+     * The constructor.
+     */
+    public HomeController() {
+        userFacade = UserFacade.getUserFacadeInstance();
+    }
+
+
+    @FXML
+    public void profileImageClicked(MouseEvent event) throws IOException {
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("../view/subscription.fxml"));
+        Scene tableViewScene = new Scene(tableViewParent);
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+        window.show();
+    }
+
+
+
+
+
 }
