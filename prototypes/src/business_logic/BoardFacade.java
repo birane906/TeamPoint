bacea7c6ef2dc132832e94e3fde7057123f360e9
tirgue <55982589+tirgue@users.noreply.github.com>
2021-01-02 15:25:52 +0100

    feat, docs : All methods implemeted and documented

--- prototypes/src/business_logic/BoardFacade.java
+++ prototypes/src/business_logic/BoardFacade.java
@@ -3,157 +3,187 @@
  *******************************************************************************/
 package business_logic;
 
-import business_logic.BoardFacade;
-import business_logic.workspace.Workspace;
-import business_logic.board.Board;
-import business_logic.board.Item;
-import business_logic.board.ItemCollection;
-// Start of user code (user defined imports)
-
-// End of user code
-
 /**
- * Description of BoardFacade.
- * 
- * @author 
+ * {@link BoardFacade} is a Singleton class. Simplify the use of
+ * buisness logic subsystem for the GUI layer. Facade pattern.
+ * Contains the buisness methods.
+ * @author Salim Azharhoussen, Birane Ba, Raphael Bourret, Nicolas Galois
  */
 public class BoardFacade {
+
 	/**
-	 * Description of the property currentBoard.
+	 * The current {@link Board} loaded in the {@link BoardFacade}
 	 */
 	public Board currentBoard = null;
 
-	// Start of user code (user defined attributes for BoardFacade)
-
-	// End of user code
+	/**
+	 * private constructor
+	 */
+	private BoardFacade() {}
 
 	/**
-	 * The constructor.
+	 * The {@link BoardFacade} <code>static</code> nested class
+	 * guarantees the uniqueness of {@link BoardFacade} instance
 	 */
-	public BoardFacade() {
-		// Start of user code constructor for BoardFacade)
-		super();
-		// End of user code
+	private static class BoardFacadeHolder {
+		/**
+		 * The unique instance of {@link BoardFacade}
+		 */
+		private static final BoardFacade BOARD_FACADE = new BoardFacade();
 	}
 
 	/**
-	 * Description of the method getBoardFacadeInstance.
-	 * @return 
+	 * <code>static</code> method. Gives the unique instance of {@link BoardFacade}
+	 * @return The {@link BoardFacade}
 	 */
 	public static BoardFacade getBoardFacadeInstance() {
-		// Start of user code for method getBoardFacadeInstance
-		BoardFacade getBoardFacadeInstance = null;
-		return getBoardFacadeInstance;
-		// End of user code
+		return BoardFacadeHolder.BOARD_FACADE;
 	}
 
 	/**
-	 * Description of the method createBoard.
-	 * @param name 
-	 * @param workspace 
-	 * @return 
+	 * Asks for {@link BoardDAO} to create a {@link Board}
+	 * @param name The new {@link Board}'s name. Can't be blank
+	 * @param workspace The {@link Workspace} to which belongs the created {@link Board}
+	 * @return <code>true</code> if the creation succeed, <code>false</code> otherwise
 	 */
 	public Boolean createBoard(String name, Workspace workspace) {
-		// Start of user code for method createBoard
-		Boolean createBoard = Boolean.FALSE;
-		return createBoard;
-		// End of user code
+		if (name.isBlank() or workspace == null) {
+			return false;
+		}
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		BoardDAO boardDAO = daoFactory.createBoardDAO();
+
+		board = boardDAO.createBoard(name, workspace);
+
+		if (boardDAO != null) {
+			currentBoard = board;
+			return true;
+		}
+		else {
+			return false;
+		}
+
 	}
 
 	/**
-	 * Description of the method deleteBoard.
-	 * @param board 
-	 * @return 
+	 * Asks for {@link BoardDAO} to delete a {@link Board}
+	 * @param board The {@link Board} to be deleted
+	 * @return <code>true</code> if the deletion succeed, <code>false</code> otherwise
 	 */
 	public Boolean deleteBoard(Board board) {
-		// Start of user code for method deleteBoard
-		Boolean deleteBoard = Boolean.FALSE;
-		return deleteBoard;
-		// End of user code
+		if (board == null) {
+			return false;
+		}
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		BoardDAO boardDAO = daoFactory.createBoardDAO();
+		
+		return boardDAO.deleteBoard(board)
 	}
 
 	/**
-	 * Description of the method retrieveBoard.
-	 * @param board 
-	 * @return 
+	 * Asks for {@link BoardDAO} to load a {@link Board} in the <code>currentBoard</code> of the {@link BoardFacade}
+	 * @param board The {@link Board} to be loaded
+	 * @return <code>true</code> if the load succeed, <code>false</code> otherwise
 	 */
 	public Boolean retrieveBoard(Board board) {
-		// Start of user code for method retrieveBoard
-		Boolean retrieveBoard = Boolean.FALSE;
-		return retrieveBoard;
-		// End of user code
+		if (board == null) {
+			return false;
+		}
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		BoardDAO boardDAO = daoFactory.createBoardDAO();
+
+		board = boardDAO.retrieveBoard(board);
+
+		if (board != null) {
+			currentBoard = board;
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	/**
-	 * Description of the method addItemCollection.
-	 * @param itemCollection 
-	 * @return 
+	 * Asks for {@link BoardDAO} to create an {@link ItemCollection}
+	 * @param itemCollectionName The new {@link ItemCollection}'s name. Can't be blank
+	 * @param board The {@link Board} to which belongs the created {@link ItemCollection}
+	 * @return <code>true</code> if the creation succeed, <code>false</code> otherwise
 	 */
-	public Boolean addItemCollection(ItemCollection itemCollection) {
-		// Start of user code for method addItemCollection
-		Boolean addItemCollection = Boolean.FALSE;
-		return addItemCollection;
-		// End of user code
+	public Boolean addItemCollection(String itemCollectionName, Board board) {
+		if (itemCollectionName.isBlank() or board == null) {
+			return false;
+		}
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		BoardDAO boardDAO = daoFactory.createBoardDAO();
+
+		ItemCollection itemCollection = boardDAO.addItemCollection(itemCollectionName, board);
+
+		if (itemCollection != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	/**
-	 * Description of the method deleteItemCollection.
-	 * @param itemCollection 
-	 * @return 
+	 * Asks for {@link BoardDAO} to delete an {@link ItemColection}
+	 * @param itemCollection The {@link ItemCollection} to be deleted
+	 * @return <code>true</code> if the deletion succeed, <code>false</code> otherwise
 	 */
 	public Boolean deleteItemCollection(ItemCollection itemCollection) {
-		// Start of user code for method deleteItemCollection
-		Boolean deleteItemCollection = Boolean.FALSE;
-		return deleteItemCollection;
-		// End of user code
+		if (itemCollection == null) {
+			return false;
+		}
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		BoardDAO boardDAO = daoFactory.createBoardDAO();
+
+		return boardDAO.deleteItemCollection(itemCollection);
 	}
 
 	/**
-	 * Description of the method addItem.
-	 * @param itemLabel 
-	 * @param itemCollection 
-	 * @return 
+	 * Asks for {@link BoardDAO} to create an {@link Item}
+	 * @param item The new {@link Item}'s name. Can't be blank
+	 * @param itemCollection The {@link ItemCollection} to which belongs the created {@link Item}
+	 * @return <code>true</code> if the creation succeed, <code>false</code> otherwise
 	 */
 	public Boolean addItem(String itemLabel, ItemCollection itemCollection) {
-		// Start of user code for method addItem
-		Boolean addItem = Boolean.FALSE;
-		return addItem;
-		// End of user code
+		if (itemLabel.isBlank() or itemCollection == null) {
+			return false;
+		}
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		BoardDAO boardDAO = daoFactory.createBoardDAO();
+
+		Item item = boardDAO.addItem(itemLabel, itemCollection);
+
+		if (item != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	/**
-	 * Description of the method deleteItem.
-	 * @param item 
-	 * @return 
+	 * Asks for {@link BoardDAO} to delete an {@link Item}
+	 * @param item The {@link Item} to be deleted
+	 * @return <code>true</code> if the deletion succeed, <code>false</code> otherwise
 	 */
 	public Boolean deleteItem(Item item) {
-		// Start of user code for method deleteItem
-		Boolean deleteItem = Boolean.FALSE;
-		return deleteItem;
-		// End of user code
+		if (item == null) {
+			return false;
+		}
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		BoardDAO boardDAO = daoFactory.createBoardDAO();
+
+		return boardDAO.deleteItem(item);
 	}
 
-	// Start of user code (user defined methods for BoardFacade)
-
-	// End of user code
 	/**
-	 * Returns currentBoard.
-	 * @return currentBoard 
+	 * @return The current board holded by the {@link BoardFacade}
 	 */
 	public Board getCurrentBoard() {
 		return this.currentBoard;
 	}
 
-	/**
-	 * Sets a value to attribute currentBoard. 
-	 * @param newCurrentBoard 
-	 */
-	/*
-	public void setCurrentBoard(Board newCurrentBoard) {
-		if (this.currentBoard != null) {
-			this.currentBoard.set(null);
-		}
-		this.currentBoard.set(this);
-	}
-	 */
 }
