5ebb5f386129bdc6b2cadbcdfa500c0870cee46a
Salim_Azh <azharhoussen.salim@gmail.com>
2021-01-05 11:31:02 +0100

    fix constructor

--- prototypes/src/business_logic/board/types/TypeFactory.java
+++ prototypes/src/business_logic/board/types/TypeFactory.java
@@ -1,20 +1,27 @@
 package business_logic.board.types;
 
 public class TypeFactory {
+    /**
+     * 
+     * @param id
+     * @param name must be the Class class.getSimpleName()
+     * @param description
+     * @return
+     */
     public static Type createType(int id, String name, String description){
         switch (name) {
             case "DateType":
-                return new DateType(id, name, description);
+                return new DateType(id, description);
             case "DependencyType":
-                return new DependencyType(id, name, description);
+                return new DependencyType(id, description);
             case "NumberType":
-                 return new NumberType(id, name, description);
+                 return new NumberType(id, description);
             case "PersonType":
-                return new PersonType(id, name, description);
+                return new PersonType(id, description);
             case "StatusType":
-                return new StatusType(id, name, description);
+                return new StatusType(id, description);
             case "TimelineType":
-                return new TimelineType(id, name, description);
+                return new TimelineType(id, description);
             default:
                 return null;
         }
