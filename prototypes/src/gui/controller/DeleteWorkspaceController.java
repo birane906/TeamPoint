be7280bcafc1d54f92eb615bd94975bc429c51c5
birane906 <birane906@gmail.com>
2021-01-05 18:40:52 +0100

    features

--- prototypes/src/gui/controller/DeleteWorkspaceController.java
+++ prototypes/src/gui/controller/DeleteWorkspaceController.java
@@ -2,17 +2,31 @@ package gui.controller;
 
 import business_logic.UserFacade;
 import business_logic.WorkspaceFacade;
+import business_logic.board.Board;
+import business_logic.board.ItemCollection;
+import business_logic.workspace.Workspace;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ResourceBundle;
 
 /**
  * Description of WorkspaceController.
@@ -23,6 +37,9 @@ public class DeleteWorkspaceController {
 
 	@FXML
 	public TextField workspaceName;
+
+	@FXML
+	public ListView workspaceListView;
 	/**
 	 * Description of the property userFacade.
 	 */
@@ -40,16 +57,18 @@ public class DeleteWorkspaceController {
 	 */
 	@FXML
 	public void validateOnAction(ActionEvent event) throws IOException{
-		if(!workspaceName.getText().isBlank()) {
-			workspaceFacade.createWorkspace(workspaceName.getText());
-			Parent tableViewParent = FXMLLoader.load(getClass().getResource("../view/workspace.fxml"));
-			Scene tableViewScene = new Scene(tableViewParent);
-			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-			window.setScene(tableViewScene);
-			window.show();
-		}else{
-			messageLabel.setText("Changement non effectué");
-		}
+		HashSet<Workspace> workspaces = userFacade.getWorkspaces();
+		ObservableList<Workspace> myWorkspaces = FXCollections.observableArrayList(workspaces);
+		workspaceListView.setItems(myWorkspaces);
+		workspaceListView.setCellFactory(lv -> new SimpleListCell());
+
+		workspaceListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				Workspace currentWorkspace = (Workspace) workspaceListView.getSelectionModel().getSelectedItem();
+				workspaceFacade.deleteWorkspace(currentWorkspace);
+			}
+		});
 	}
 
 	/**
@@ -73,8 +92,6 @@ public class DeleteWorkspaceController {
 		userFacade = UserFacade.getUserFacadeInstance();
 		workspaceFacade = WorkspaceFacade.getWorkspaceFacadeInstance();
 	}
-
-
 
 
 }
