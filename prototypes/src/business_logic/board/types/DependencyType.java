51735d7d2f1c04e7fa1038b1354b4be0e5b43f8e
Salim_Azh <azharhoussen.salim@gmail.com>
2021-01-05 11:22:45 +0100

    docs feat contructor add remove item refactor Set

--- prototypes/src/business_logic/board/types/DependencyType.java
+++ prototypes/src/business_logic/board/types/DependencyType.java
@@ -1,37 +1,49 @@
 package business_logic.board.types;
 
 import java.util.HashSet;
+import java.util.Set;
 
 import business_logic.board.Item;
 
 /**
  * Description of DependencyType.
  * 
- * @author 
+ * @author Salim Azharhoussen, Birane Ba, Raphael Bourret, Nicolas Galois
  */
 public class DependencyType extends Type  {
 	/**
-	 * Description of the property items.
+	 * The items that must be completed before the current item
 	 */
-	public HashSet<Item> items = new HashSet<Item>();
+	public Set<Item> items;
 
 	/**
-	 * The constructor.
+	 * Constructor  for empty {@link DependencyType}
 	 *
 	 * @param id
-	 * @param nameType
 	 * @param description
 	 */
-	public DependencyType(int id, String nameType, String description) {
-		super(id, nameType, description);
+	public DependencyType(int id, String description) {
+		super(id, DependencyType.class.getSimpleName(), description);
+		items = new HashSet<Item>();
 	}
 
+	/**
+	 * Constructor  for empty {@link DependencyType}
+	 * @param id
+	 * @param description
+	 * @param items
+	 */
+	public DependencyType(int id, String description, Set<Item> items) {
+		super(id, DependencyType.class.getSimpleName(), description);
+		this.items = items;
+	}
 
 	/**
 	 * Description of the method addItem.
 	 * @param item 
 	 */
 	public void addItem(Item item) {
+		this.items.add(item);
 	}
 	 
 	/**
@@ -39,13 +51,14 @@ public class DependencyType extends Type  {
 	 * @param item 
 	 */
 	public void removeItem(Item item) {
+		this.items.remove(item);
 	}
 
 	/**
 	 * Returns items.
 	 * @return items 
 	 */
-	public HashSet<Item> getItems() {
+	public Set<Item> getItems() {
 		return this.items;
 	}
 }
