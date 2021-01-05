d24db2c9423e6cd840cb9c20f02e54bc0a2757ad
birane906 <birane906@gmail.com>
2021-01-05 16:59:45 +0100

    features

--- prototypes/src/gui/controller/ProfileController.java
+++ prototypes/src/gui/controller/ProfileController.java
@@ -5,32 +5,53 @@ import business_logic.UserFacade;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Description of HomeController.
  *
  *
  */
-public class ProfileController {
+public class ProfileController implements Initializable {
+    @FXML
+    public Label editNameLabel;
+
+    @FXML
+    public Label editFirstNameLabel;
+
+    @FXML
+    public Label editEmailLabel;
+
+    @FXML
+    public Label editDescriptionLabel;
+
+    @FXML
+    public Label editBirthdayLabel;
+
+    @FXML
+    public Label editNumberLabel;
     /**
      * Description of the property userFacade.
      */
     private UserFacade userFacade;
 
 
-    /**
-     * Description of the close button
-     */
-    @FXML
-    private Button closeButton;
+
+
+
 
 
 
@@ -153,13 +174,6 @@ public class ProfileController {
     }
 
 
-    /**
-     * Method which permite to close the window when you click on the close button
-     */
-    public void closeButtonOnAction(){
-        Stage stage = (Stage) closeButton.getScene().getWindow();
-        stage.close();
-    }
 
 
     @FXML
@@ -169,5 +183,19 @@ public class ProfileController {
         Stage window = (Stage) ((Node)mouseEvent.getSource()).getScene().getWindow();
         window.setScene(tableViewScene);
         window.show();
+    }
+
+    /**
+     * Method which initialize the login window
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        editBirthdayLabel.setText(userFacade.getCurrentUser().getBirthday().toString());
+        editDescriptionLabel.setText(userFacade.getCurrentUser().getProfileDescription());
+        editEmailLabel.setText(userFacade.getCurrentUser().getEmail());
+        editFirstNameLabel.setText(userFacade.getCurrentUser().getFirstName());
+        editNameLabel.setText(userFacade.getCurrentUser().getName());
+        editNumberLabel.setText(userFacade.getCurrentUser().getPhoneNumber());
+
     }
 }
