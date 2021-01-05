8846841012cc03d4ab786b34a441e3d24a16b231
birane906 <birane906@gmail.com>
2021-01-05 06:05:00 +0100

    features

--- prototypes/src/gui/controller/WorkspaceController.java
+++ prototypes/src/gui/controller/WorkspaceController.java
@@ -58,6 +58,9 @@ public class WorkspaceController implements Initializable {
 	@FXML
 	public ListView<Board> listBoard;
 
+	@FXML
+	public ScrollPane sp;
+
 	/**
 	 * Description of the property userFacade.
 	 */
@@ -147,6 +150,7 @@ public class WorkspaceController implements Initializable {
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 		line.setVisible(false);
 		line2.setVisible(false);
+		sp.setVisible(false);
 		addBoardImage.setVisible(false);
 		addBoardLabel.setVisible(false);
 		inviteMemberImage.setVisible(false);
@@ -168,6 +172,7 @@ public class WorkspaceController implements Initializable {
 
 							workspaces.setText(m.getText());
 							workspaceName.setText(m.getText());
+							sp.setVisible(true);
 							line.setVisible(true);
 							line2.setVisible(true);
 							addBoardImage.setVisible(true);
