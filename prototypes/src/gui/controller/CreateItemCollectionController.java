212abbb6fc0cd1892e87af5bf7edcc2aa7bc6ed9
birane906 <birane906@gmail.com>
2021-01-11 19:41:39 +0100

    features : creation of workspace good

---
+++ prototypes/src/gui/controller/CreateItemCollectionController.java
@@ -0,0 +1,86 @@
+package gui.controller;
+
+import business_logic.BoardFacade;
+import business_logic.UserFacade;
+import business_logic.WorkspaceFacade;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * Description of WorkspaceController.
+ * 
+ * @author 
+ */
+public class CreateItemCollectionController {
+
+	@FXML
+	public TextField itemCollectionName;
+	/**
+	 * Description of the property userFacade.
+	 */
+	private UserFacade userFacade;
+
+	private WorkspaceFacade workspaceFacade;
+
+	private BoardFacade boardFacade;
+
+
+
+	@FXML
+	private Label messageLabel;
+
+	/**
+	 * Method which permite an user to validate his first name modification
+	 * @param event
+	 */
+	@FXML
+	public void validateOnAction(ActionEvent event) throws IOException{
+		if(!boardName.getText().isBlank()) {
+
+			boardFacade.createBoard(boardName.getText(),workspaceFacade.getCurrentWorkspace());
+			Parent tableViewParent = FXMLLoader.load(getClass().getResource("../view/workspace.fxml"));
+			Scene tableViewScene = new Scene(tableViewParent);
+			Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			window.setScene(tableViewScene);
+			window.show();
+		}else{
+			messageLabel.setText("Changement non effectué");
+		}
+	}
+
+	/**
+	 * Method which permite an user to cancel his first name modification
+	 * @param event
+	 */
+	@FXML
+	public void cancelOnAction(ActionEvent event) throws IOException{
+		Parent tableViewParent = FXMLLoader.load(getClass().getResource("../view/workspace.fxml"));
+		Scene tableViewScene = new Scene(tableViewParent);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(tableViewScene);
+		window.show();
+	}
+
+	/**
+	 * The constructor.
+	 */
+	public CreateItemCollectionController() {
+		super();
+		userFacade = UserFacade.getUserFacadeInstance();
+		workspaceFacade = WorkspaceFacade.getWorkspaceFacadeInstance();
+		boardFacade = BoardFacade.getBoardFacadeInstance();
+	}
+
+
+
+
+}
